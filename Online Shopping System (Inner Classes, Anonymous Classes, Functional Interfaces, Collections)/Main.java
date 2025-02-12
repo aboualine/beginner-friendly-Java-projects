e359532b2bde9c355476fa6d2e101b2bd304b477
ABOUALINE MOHAMED <mohamedaboualine@gmail.com>
2025-02-12 23:40:20 +0100

    Commit 12/02/2025 depart de creation de classe main

--- Online Shopping System (Inner Classes, Anonymous Classes, Functional Interfaces, Collections)/Main.java
+++ Online Shopping System (Inner Classes, Anonymous Classes, Functional Interfaces, Collections)/Main.java
@@ -1,3 +1,25 @@
+import java.util.Scanner;
+
 public class Main {
-    
+    public static void main(String[] args) {
+        System.out.println("Hi! and welcome to my online shoping system :)");
+        Scanner write = new Scanner(System.in);
+        while (true) {
+            System.out.println("-----Online Shoping System-----");
+            System.out.println("1- Display Products.");
+            System.out.println("2- Add Product.");
+            System.out.println("3- Remove Product.");
+            System.out.println("4- Apply Discounts.");
+            System.out.println("5- Display Products.");
+            System.out.println("6- Choose Payments.");
+            System.out.println("7- Checkout.");
+            System.out.print("Enter your choice");
+            if(!write.hasNext()){
+                System.out.println("enter a number between 1 and 7 !");
+                write.next();
+                continue;
+            }
+            
+        }
+    }
 }
